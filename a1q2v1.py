71f4be74b1f48fa49acb39920481c06858fd016c
QDB802 <122410975+QDB802@users.noreply.github.com>
2023-01-18 09:46:02 -0600

    a1q2v1

--- a1q2v1.py
+++ a1q2v1.py
@@ -1,34 +1,66 @@
 # Name: Ken Duff
 # NSID: qdb802
 # Student#: 11318955
+# Class: CMPT 141
 # Instructor: Gang Li
 
 
 def baseboard(width, length):
-    b_cost = int(input("What is the cost of one linear foot of baseboard ($): "))
-    x = int(((2 * width) + (2 * length)) * b_cost)
+    """
+    Purpose:
+        Calculates the cost of putting baseboards in the entire room.
+    Parameters:
+        width: Width of the specified room.
+        length: Length of the specified room.
+    Returns:
+        The cost required for the baseboards of the specified room.
+    """
+    x = int(((2 * width) + (2 * length)) * int(input("What is the cost of one linear foot of baseboard ($): ")))
     return x
 
 
 def carpet(width, length):
-    c_cost = int(input("What is the cost of one square foot of carpet ($): "))
-    y = int((width * length) * c_cost)
+    """
+    Purpose:
+        Calculates the cost of carpeting the entire room.
+    Parameters:
+        width: Width of the specified room.
+        length: Length of the specified room.
+    Returns:
+        The cost required for the carpet of the specified room.
+    """
+    y = int((width * length) * int(input("What is the cost of one square foot of carpet ($): ")))
     return y
 
 
-def cost(x, y):
+def cost(width, length):
+    """
+    Purpose:
+        Calculates the TOTAL cost of renovating the entire room.
+    Parameters:
+        width: Width of the specified room.
+        length: Length of the specified room.
+    Returns:
+        The total cost of baseboards, carpets, and a flat labor cost of $500.
+    """
+    x = baseboard(width, length)
+    y = carpet(width, length)
     c = float(x + y + 500)
     return c
 
 
 def main():
+    """
+    Purpose:
+        Initializes the program, runs the cost() function, and then prints our resulting width, length, and total
+        renovation costs.
+    """
     width = int(input("What is the width of the room (ft): "))
     length = int(input("What is the length of the room (ft): "))
-    x = baseboard(width, length)
-    y = carpet(width, length)
-    c = cost(x, y)
-    print("For a room of width", float(width), "and length of", float(length), "the cost of the renovation would be $",
-          c)
+    c = cost(width, length)
+    print(" ")
+    print("For a room of width", float(width), "feet and length of", float(length), "feet, the cost of the renovation"
+          , "would be $", c)
 
 
 main()
